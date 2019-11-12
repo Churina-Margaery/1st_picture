ee7847e6cdec20b9dd35f07da148e8c816c757c2
Churina-Margaery <55570151+Churina-Margaery@users.noreply.github.com>
2019-11-12 22:21:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,49 @@
 # - Novikov Dmitriy
 # - Churina Margaery
 
-def rect(x, y, side1, side2, color):
-    pass
-  # Симон, тут ты пишешь функцию рисования прямоугольника с координатами (правый верхний угол) x, y, и сторонами side1, side2
-  
-def trian(x, y, side1, side2, side3, color):
-    pass
-  # Симон, тут ты пишешь функцию рисования треугольника с координатами (правый верхний угол) x, y, и сторонами side1, side2, side3
+import turtle
 
-def circ(x, y, side1, color):
+def rect(x, y, side1, side2, angle_of_rotation, color):
     pass
-  # Я тут пишу функцию рисования круга с координатами центра x, y, и радиусом side1
-  
-def square(x, y, side1, color):
+
+
+# Симон, тут ты пишешь функцию рисования прямоугольника с координатами (правый верхний угол) x, y, и сторонами side1, side2 и углом поворота
+
+def trian(x, y, side1, side2, side3, angle_of_rotation, color):
     pass
-  # я тут пишу функцию рисования квадрата с координатами (правый верхний угол) x, y, и стороной side1
+
+
+# Симон, тут ты пишешь функцию рисования треугольника с координатами (правый верхний угол) x, y, и сторонами side1, side2, side3 и углом поворота
+
+def circ(x, y, side, color):
+    x_1 = x - side
+    y_1 = y - side
+    turtle.up()
+    turtle.goto(x_1, y_1)
+    turtle.down()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.circle(side)
+    turtle.end_fill()
+
+
+def square(x, y, side, angle_of_rotation, color):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.right(angle_of_rotation)
+    turtle.forward(side)
+    for _ in range(4):
+        turtle.forward(side)
+        turtle.right(90)
+    turtle.end_fill()
+
 
 def rhom(x, y, side, angle, angle_of_rotation, color):
     turtle.up()
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.down()
     turtle.color(color)
     turtle.begin_fill()
@@ -34,8 +58,10 @@ def rhom(x, y, side, angle, angle_of_rotation, color):
         turtle.right(360 - angle)
     turtle.left(angle)
     turtle.end_fill()
-  # где x и y - координаты, side - сторона ромба, angle - один из углов ромба, angle_of_rotation - на сколько повернуть фигуру
-  
+
+
+# где x и y - координаты, side - сторона ромба, angle - один из углов ромба, angle_of_rotation - на сколько повернуть фигуру
+
 def trap(x, y, bigger_base, side, angle, angle_of_rotation, color):
     turtle.up()
     turtle.goto(x, y)
@@ -47,31 +73,35 @@ def trap(x, y, bigger_base, side, angle, angle_of_rotation, color):
     turtle.left(180 - angle)
     turtle.forward(side)
     turtle.left(angle)
-    turtle.forward(bigger_base - 2*math.cos(math.radians(angle))*side)
+    turtle.forward(bigger_base - 2 * math.cos(math.radians(angle)) * side)
     turtle.left(angle)
     turtle.forward(side)
     turtle.end_fill()
-  # где x и y - координаты, bigger_base - большее основание трапеции, side - боковая сторона трапеции, angle - угол между ними, angle_of_rotation - на сколько повернуть фигуру
 
-    
-    
+
+# где x и y - координаты, bigger_base - большее основание трапеции, side - боковая сторона трапеции, angle - угол между ними, angle_of_rotation - на сколько повернуть фигуру
+
+
 # картинки
 def house(f1, f2):
-    pass # (Симон)
+    pass  # (Симон)
+
 
 def fish(f1, f2):
-    pass # (Симон)
+    pass  # (Симон)
 
 
 def ship(f1, f2):
-    pass # (Дима)
+    pass  # (Дима)
+
 
 def Catepilar(f1, f2, f3, f4, f5):
-    pass # (Дима)
+    pass  # (Дима)
 
 
 def tree(f1, f2, f3):
-    pass # (Я)
+    pass  # (Я)
+
 
 def tree_2(f1, f2):
-    pass # (Я)
+    pass  # (Я)
